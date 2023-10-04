75ad9c1fc2df4727a5115ef256c00f7b71ca50ed
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-10-04 12:15:31 +0530

    Installed a template for customer's UI.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="main py-4">
+        <div class="row">
+            <div class="col-12 col-xl-12">
+                <div class="col-12 px-0">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            <h2 class="fs-5 fw-bold mb-1">{{ __('About us') }}</h2>
+                            <p>{{ __('Sample static text page ') }}</p>
+                            <label class="my-1 me-2" for="country">Country</label>
+                            <select class="form-select" id="country" aria-label="Default select example">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
