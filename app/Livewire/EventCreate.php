137eb2a08d9820966276622f3c498a1d34a64031
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-10-08 20:19:37 +0530

    Created event creation livewire component.

--- app/Livewire/EventCreate.php
+++ app/Livewire/EventCreate.php
@@ -2,6 +2,13 @@
 
 namespace App\Livewire;
 
+use App\Models\Day;
+use App\Models\Event;
+use App\Models\Showtime;
+use DateInterval;
+use DatePeriod;
+use DateTime;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class EventCreate extends Component
@@ -9,14 +16,72 @@ class EventCreate extends Component
 
     public $startDate;
     public $endDate;
-
     public bool $areDatesFilled = false;
+    public bool $areShowTimesFilled = false;
+    public Collection $showTimes;
+    public array $showTimesArray = [];
+    public string $eventName;
 
     public function openShowtime()
     {
         if ($this->startDate && $this->endDate) {
             $this->areDatesFilled = true;
+            $this->showTimes = Showtime::select(['id', 'start_time', 'end_time'])
+                ->get();
         }
+    }
+
+    public function addShowtimes($showtimeId)
+    {
+        // if shpwtimeid contains in array then remove it if not then add it
+        if (in_array($showtimeId, $this->showTimesArray)) {
+            $this->showTimesArray = array_diff($this->showTimesArray, [$showtimeId]);
+        } else {
+            $this->showTimesArray[] = $showtimeId;
+        }
+
+    }
+
+    public function addName()
+    {
+        $this->areShowTimesFilled = true;
+    }
+
+    public function createEvents()
+    {
+        $dayId = 0;
+        $venueId = auth()->user()->venue->id;
+
+        // all dates between start and end date
+        $begin = new DateTime( $this->startDate);
+        $end = new DateTime( $this->endDate);
+        $end = $end->modify( '+1 day' );
+
+        $interval = new DateInterval('P1D');
+        $period = new DatePeriod($begin, $interval ,$end);
+
+        foreach ($period as $key => $date) {
+           if (Day::where('date', $date)->exists()) {
+               $dayId = Day::where('date', $date)->first()->id;
+           } else {
+                $day = new Day();
+                $day->date = $date;
+                $day->save();
+                $dayId = $day->id;
+           }
+
+           foreach ($this->showTimesArray as $showTimeId) {
+               $event = new Event();
+               $event->name = $this->eventName;
+               $event->day_id = $dayId;
+               $event->showtime_id = $showTimeId;
+               $event->venue_id = $venueId;
+               $event->save();
+           }
+        }
+
+        toast('Events Created!','success');
+        $this->redirect(route('events.create'));
     }
 
     public function render()
--- resources/views/livewire/event-create.blade.php
+++ resources/views/livewire/event-create.blade.php
@@ -11,22 +11,48 @@
                         <label for="startdate">Start Date</label>
                         <input type="date" name="startdate" class="form-control"
                                id="startdate"
-                               required wire:model.live="startDate">
+                               required wire:model.blur="startDate">
 
                     </div>
                     <div class="mb-3">
                         <label for="enddate">End Date</label>
                         <input type="date" name="enddate"
-                               class="form-control" id="enddate" wire:model.live="endDate" required>
+                               class="form-control" id="enddate" wire:model.blur="endDate" required>
 
                     </div>
 
-                    <button wire:click.prevent="openShowtime()" class="btn btn-tertiary">Next</button>
+                    @if(!$areDatesFilled)
+                        <button wire:click.prevent="openShowtime()" class="btn btn-tertiary">Next</button>
+                    @endif
 
                     @if($areDatesFilled)
-
-                        <input type="text" name="" id="">
+                        <label>Select Show Times</label>
+                        @foreach($showTimes as $showtime)
+                            <div class="form-check">
+                                <input wire:click="addShowtimes({{ $showtime->id }})" class="form-check-input"
+                                       type="checkbox" value="{{ $showtime->id }}" id="showTime{{ $showtime->id }}">
+                                <label class="form-check-label" for="showTime{{ $showtime->id }}">
+                                    {{ $showtime->start_time }} - {{ $showtime->end_time }}
+                                </label>
+                            </div>
+                        @endforeach
+                        @if(!$areShowTimesFilled)
+                            <button wire:click.prevent="addName()" class="btn btn-tertiary">Next</button>
+                        @endif
                     @endif
+
+                    @if($areDatesFilled && $areShowTimesFilled)
+
+                        <div class="mb-4 mt-3">
+                            <label for="eventname">Event Name</label>
+                            <input type="text" name="eventname" class="form-control"
+                                   id="eventname"
+                                   required wire:model.blur="eventName">
+
+                        </div>
+                        <button wire:click.prevent="createEvents()" class="btn btn-tertiary">Save</button>
+                    @endif
+
                 </form>
             </div>
         </div>
