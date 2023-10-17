234b8dfa68d4823f4c6c0688193725ff45318e14
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-10-17 20:23:16 +0530

    Wrote tests for login and home.

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -10,16 +10,39 @@ it('redirects to home if logged in', function () {
 
     $user = User::factory()->create();
 
-    $response = $this->actingAs($user)->get('/login');
-
-    $response->assertRedirect('/home');
+    $this->actingAs($user)
+        ->get('/login')
+        ->assertRedirect('/home');
 });
 
 it('redirects to login if not logged in', function () {
 
-    $response = $this->get('/home');
+    $this->get('/home')
+        ->assertDontSeeText(['Create Venue', 'Update Venue', 'Events', 'Create Event', 'Manage Events'])
+        ->assertRedirect('/login');
+});
 
-    $response->assertRedirect('/login');
+it('shows create venue on navbar if logged in user not have a venue', function () {
+
+    $user = User::factory()->create();
+
+    $this->actingAs($user)->get('/home')
+        ->assertSee(['Create Venue', $user->name])
+        ->assertDontSeeText(['Update Venue', 'Events', 'Create Event', 'Manage Events']);
+});
+
+it('shows update venue on navbar if logged in user have a venue', function () {
+
+    $user = User::factory()->create();
+    $user->venue()->create([
+        'user_id' => $user->id,
+        'name' => 'Venue Name',
+        'seating_capacity' => 10,
+    ]);
+
+    $this->actingAs($user)->get('/home')
+        ->assertSeeText(['Update Venue', 'Events', 'Create Event', 'Manage Events', $user->name])
+        ->assertDontSeeText(['Create Venue']);
 });
 
 
