8e24dbee6f4113b8d8c36cb0e344a6155398a216
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-10-17 18:28:19 +0530

    Added code for fix sqlite foreign key drop error.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,49 @@
 
 namespace Tests;
 
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\SQLiteBuilder;
+use Illuminate\Database\SQLiteConnection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Fluent;
+use Pest\Support\Closure;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        $this->hotfixSqlite();
+        parent::__construct($name, $data, $dataName);
+    }
+
+    public function hotfixSqlite()
+    {
+        \Illuminate\Database\Connection::resolverFor('sqlite',
+            function ($connection, $database, $prefix, $config) {
+                return new class($connection, $database, $prefix, $config)
+                    extends SQLiteConnection {
+                    public function getSchemaBuilder()
+                    {
+                        if ($this->schemaGrammar === null) {
+                            $this->useDefaultSchemaGrammar();
+                        }
+
+                        return new class($this) extends SQLiteBuilder {
+                            protected function createBlueprint($table, Closure|\Closure $callback = null)
+                            {
+                                return new class($table, $callback) extends Blueprint {
+                                    public function dropForeign($index)
+                                    {
+                                        return new Fluent();
+                                    }
+                                };
+                            }
+                        };
+                    }
+                };
+            });
+    }
 }
+
