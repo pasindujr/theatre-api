5eac6ccf805bf0c509aae793f979f474f6a62b17
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-10-19 12:21:54 +0530

    Updates to events show table.

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="main py-4">
         <div class="card card-body border-0 shadow table-wrapper table-responsive">
-            <h2 class="mb-4 h5">{{ __('Create the venue') }}</h2>
+            <h2 class="mb-4 h5">{{ __('Events') }}</h2>
 
             <div class="container row justify-content-center">
 
@@ -25,7 +25,25 @@
                                 <td>{{ $event->day->date }}</td>
                                 <td>{{ $event->showtime->start_time }}</td>
                                 <td>{{ $event->showtime->end_time }}</td>
-                                <td><a href="{{ route('events.show', $event->id) }}">Show</a></td>
+                                <td>
+                                    <div class="btn-group">
+                                        <a href="{{ route('events.show', $event->id) }}" class="btn btn-primary">More</a>
+                                        <button type="button"
+                                                class="btn btn-primary dropdown-toggle dropdown-toggle-split"
+                                                data-bs-toggle="dropdown" aria-expanded="false">
+                                            <svg class="icon icon-xs" fill="currentColor" viewBox="0 0 20 20"
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                      clip-rule="evenodd"></path>
+                                            </svg>
+                                            <span class="visually-hidden">Toggle Dropdown</span>
+                                        </button>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Endpoint</a></li>
+                                        </ul>
+                                    </div>
+                                </td>
                             </tr>
 
                         @endforeach
