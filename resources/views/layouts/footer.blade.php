768ce18dfc0652ef3aa6080052bebdf07ac345c9
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-10-21 23:09:20 +0530

    Changes to footer and home page.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,16 +3,5 @@
         <div class="col-12 col-md-4 col-xl-6 mb-4 mb-md-0">
             <p class="mb-0 text-center text-lg-start">© 2019-<span class="current-year"></span> <span class="mb-0 text-center text-lg-start">Theatre API</span>
         </div>
-        <div class="col-12 col-md-8 col-xl-6 text-center text-lg-start">
-            <!-- List -->
-            <ul class="list-inline list-group-flush list-group-borderless text-md-end mb-0">
-                <li class="list-inline-item px-0 px-sm-2">
-                    <a href="https://themesberg.com/about">About</a>
-                </li>
-                <li class="list-inline-item px-0 px-sm-2">
-                    <a href="https://themesberg.com/contact">Contact</a>
-                </li>
-            </ul>
-        </div>
     </div>
 </footer>
